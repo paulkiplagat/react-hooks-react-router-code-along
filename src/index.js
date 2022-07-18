34c1a6176fce06ffb1391c028616c9f128057883
Paul <paul.kiplagt@student.moringaschool.com>
2022-07-18 17:40:54 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,3 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-function Home() {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-}
-
-function App() {
-  return <Home />;
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
+import { BrowserRouter as Router } from "react-router-dom";
